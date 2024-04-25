b6fd2c5ab226b7afe81e7a571160dbe2fc04c7cc
Yuri Gonçalves <yuriv.goncalves@gmail.com>
2024-04-24 22:05:15 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Yuri Gonçalves on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
