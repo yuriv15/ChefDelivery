b993d1c597677372198b8357f0ba12932534e06d
Yuri Gonçalves <yuriv.goncalves@gmail.com>
2024-04-25 22:23:10 -0300

    feat: NavigationBar

--- ChefDelivery/App/ContentView.swift
+++ ChefDelivery/App/ContentView.swift
@@ -10,15 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Teste")
+            Text("Teste")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .previewLayout(.sizeThatFits)
 }
---
+++ ChefDelivery/NavigationBar/NavigationBar.swift
@@ -0,0 +1,36 @@
+//
+//  NavigationBar.swift
+//  ChefDelivery
+//
+//  Created by Yuri Gonçalves on 24/04/24.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            Button("R. Vergueiro, 3185") {
+                
+            }
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .foregroundColor(.black)
+            
+            Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Image(systemName: "bell.badge")
+                    .font(.title3)
+                    .foregroundColor(.red)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationBar()
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
